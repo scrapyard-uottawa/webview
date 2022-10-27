91ae404afacc0db6fc7bcd3e8486ddb1ea733dca
Rachel Wendling <rwendling14@gmail.com>
2022-10-27 13:12:30 -0400

    more css

--- src/pages/Machine/components/BinCapacity/BinCapacity.js
+++ src/pages/Machine/components/BinCapacity/BinCapacity.js
@@ -1,14 +1,24 @@
-
+import './BinCapacity.css';
 export const BinCapacity = ({ props }) => {
     // if the props are not defined, return null
     if (!props) return <p>Data has not been loaded.</p>;
     return (
         <div className="bin-capacity">
+            <table>
             <h1>Bin Capacity</h1>
-            <p>Black Bin: {props.blackBin}%</p>
-            <p>Blue Bin: {props.blueBin}%</p>
-            <p>Compost: {props.compost}%</p>
-            <p>Garbage: {props.garbage}%</p>
+            <tr>
+                <th>Black Bin</th>
+                <th>Blue Bin</th>
+                <th>Compost</th>
+                <th>Garbage</th>
+            </tr>
+            <tr>
+                <td>{props.blackBin}%</td>
+                <td>{props.blueBin}%</td>
+                <td>{props.compost}%</td>
+                <td>{props.garbage}%</td>
+            </tr>
+            </table>
         </div>
     );
 }
--- src/pages/NavBar/NavBar.js
+++ src/pages/NavBar/NavBar.js
@@ -5,8 +5,12 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (  
         <div class="NavBar">
-            <nav class="navbar navbar-dark bg-dark">
+            <nav class="navbar navbar-dark bg-dark fixed-top">
             <a class="navbar-brand" href="/">ScrapYard</a>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+            </li>
             </nav>
         </div>
     );
